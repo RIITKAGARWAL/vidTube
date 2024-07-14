0299164004efb3cf1c08d9762d13814fd1ee4361
RIITKAGARWAL <agarwalritik895@gmail.com>
2024-07-14 05:37:03 +0530

    mic functionality added

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ document.querySelector(".menu-icon").addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('small-container');
 
 });
+
+var mic = document.querySelector(".nav-middle>.mic-icon");
+mic.addEventListener('click',voice);
+
+function voice(){
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-GB";
+    recognition.onresult = function(event){
+        console.log(event);
+        document.querySelector(".nav-middle>.search-box>input").value =
+        event.results[0][0].transcript;
+    }
+    recognition.start();
+}
